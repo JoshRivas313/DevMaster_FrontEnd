ee9b29de6671c148a0d075de500544621f8aec8e
Jose <hurtadorivasj123@gmail.com>
2025-11-15 00:48:51 -0500

    GIT DE PRUEBA

--- src/app/authentication/auth.routes.ts
+++ src/app/authentication/auth.routes.ts
@@ -1,13 +1,20 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
+
+    {
     path: '',
     redirectTo: 'auth/login',
     pathMatch: 'full',
   },
 
-
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./authentication/auth.routes').then(
+        m => m.AUTH_ROUTES
+      ),
+  },
 
   // página 404 (opcional)
   {
